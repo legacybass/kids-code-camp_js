1f319e3163db2c8db6326b43b76e65065b7dea10
Daniel Beus <dbeus@powerteq.com>
2015-06-15 11:56:27 -0600

    Code for a much simpler version of the game Top down scroller with a simple character and a crab

--- javascript/game.js
+++ javascript/game.js
@@ -1,59 +1,138 @@
+window.addEventListener("keydown", function (key) {
+	var guy = document.querySelector('.good-guy');
+	switch(key.which) {
+		case 40:
+			moveCharacter(guy, "Down");
+			break;
+		case 37:
+			moveCharacter(guy, "Left");
+			break;
+		case 39:
+			moveCharacter(guy, "Right");
+			break;
+		case 38:
+			moveCharacter(guy, "Up");
+			break;
+		case 32:
+			shoot();
+			break;
+	}
+});
 
-/* Game namespace */
-var game = {
-
-	// an object where to store game information
-	data : {
-		// score
-		score : 0
-	},
+function shoot() {
+	var guy = document.querySelector('.good-guy')
 	
-    // Run on page load.
-    "onload" : function () {
-        // Initialize the video.
-        if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto"})) {
-            alert("Your browser does not support HTML5 canvas.");
-            return;
-        }
+	var top = getComputedStyle(guy)["top"]
+	top = top.replace('px', '')
+	top = parseInt(top)
+	
+	var left = getComputedStyle(guy)["left"];
+	left = left.replace('px', '');
+	left = parseInt(left);
+	
+	var bullet = document.querySelector('.bullet');
+	bullet.style.top = (top - 5) + "px";
+	bullet.style.left = (left + 32) + "px"
+	
+	bullet.style.display = 'block'
+	
+	//Here is where we will move the bullet
+	//setInterval does a task repeatedly until we clear it
+	var interval = setInterval(function() {
+		//Let's grab the top value of the bullet, just like
+		//we did with the top and left above for the good-guy
+		var bulletTop = getComputedStyle(bullet)["top"];
+		bulletTop = bulletTop.replace('px', '');
+		bulletTop = parseInt(bulletTop);
 		
-        // add "#debug" to the URL to enable the debug Panel
-        if (me.game.HASH.debug === true) {
-            window.onReady(function () {
-                me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
-            });
-        }
+		//If the bullet's top is already -10px away,
+		//Let's clearInterval (stop the interval) 
+		//and set the bullet to display none so it goes
+		//invisible
+		if (bulletTop < -10) {
+			bullet.style.display = "none";
+			clearInterval(interval)
+		} else {
+			//Otherwise, move the bullet up 10px
+			bullet.style.top = (bulletTop - 10) + "px"
+		}
+	}, 5) //<-- change this number to 
+				//    make the bullet go slower orfaster
+}
 
-        // Initialize the audio.
-        me.audio.init("mp3,ogg");
-
-        // Set a callback to run when loading is complete.
-        me.loader.onload = this.loaded.bind(this);
-     
-        // Load the resources.
-        me.loader.preload(game.resources);
-
-        // Initialize melonJS and display a loading screen.
-        me.state.change(me.state.LOADING);
-    },
-
-
-
-    // Run on game resources loaded.
-    "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
+function moveCharacter(character, direction) {
+	var top = getComputedStyle(character)["top"]
+	top = top.replace('px', '')
+	top = parseInt(top)
+	
+	var left = getComputedStyle(character)["left"];
+	left = left.replace('px', '');
+	left = parseInt(left);
+	
+	var width = 300;
+	var height = 300;
+	
+	if (direction === "Down") {
+		if(top < 225) {
+			top = top + 25;
+		}
+		else {
+			top = 250;
+		}
+		character.style.top = top + "px";  
 		
-		// add our player entity in the entity pool
-		me.pool.register("mainPlayer", game.PlayerEntity);
-		me.pool.register("CoinEntity", game.CoinEntity);
-		me.pool.register("EnemyEntity", game.EnemyEntity);
-			
-		// enable the keyboard
-		me.input.bindKey(me.input.KEY.LEFT,		"left");
-		me.input.bindKey(me.input.KEY.RIGHT,	"right");
-		me.input.bindKey(me.input.KEY.X,		"jump", true);
+	} else if (direction === "Up") {
+		if(top > 25) {
+			top = top - 25;
+		}
+		else {
+			top = 0;
+		}
+		character.style.top = top + "px";
+		
+	} else if (direction === "Right") {
+		if(left < 225) {
+			left = left + 25;
+		}
+		else {
+			left = 250;
+		}
+		character.style.left = left + "px";
+		
+	} else if (direction === 'Left') {
+		if(left > 25) {
+			left = left - 25;
+		}
+		else {
+			left = 0;
+		}
+		character.style.left = left + "px";
+	}	
+}
 
-        // Start the game.
-        me.state.change(me.state.PLAY);
-    }
-};
+function moveEnemy() {
+	// This function will control moving the crab around the good guy
+	
+	// Let's get the crab and the good guy
+	var enemy = document.querySelector('.crab');
+	var goodGuy = document.querySelector('.good-guy');
+	
+	// setInterval will repeatedly call this code
+	// it will allow the crab to move on its own
+	setInterval(function () {
+		// Get a random direction for him to move
+		var randomValue = Math.floor(Math.random() * 4);
+		switch(randomValue) {
+			case 0: moveCharacter(enemy, "Up");
+				break;
+			case 1: moveCharacter(enemy, "Down");
+				break;
+			case 2: moveCharacter(enemy, "Left");
+				break;
+			case 3: moveCharacter(enemy, "Right");
+				break;
+		}
+	}, 1500)
+}
+
+moveEnemy();
